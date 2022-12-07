5303aba79d5be322891e38f8d4f5a780262a73d8
Richard <LIU22291@spengergasse.at>
2022-12-07 23:51:26 +0100

    kleine Überarbeitung

--- TripleAProject/TripleAProject.Webapi/Program.cs
+++ TripleAProject/TripleAProject.Webapi/Program.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using TripleAProject.Webapi.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-var opt = new DBContextOptionsBuilder()
+var opt = new DbContextOptionsBuilder()
     .UseMySql(
         builder.GetConnectionString("MySql"),
         new MariaDbServerVersion(new Version(10, 4, 22))
