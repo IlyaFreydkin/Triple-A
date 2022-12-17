7b0388005156930261a6f0118dd78dfc442edc0f
Ilya <fre22343@spengergasse.at>
2022-12-17 14:33:15 +0100

    17.12.2022

--- TripleAProject/TripleAProject.Client/src/App.vue
+++ TripleAProject/TripleAProject.Client/src/App.vue
@@ -1,48 +1,75 @@
 
 
 <template>
-    <div id="app">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div id="app" class="bg-dark">
+        <nav id="navbar" class="navbar navbar-expand-lg navbar-light ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+      <ul class="navbar-nav me-auto  mb-lg-0">
+        <li class="nav-item me-3 ">
+          <a class="nav-link active" id="nav-logo-font" aria-current="page" href="#">Triple A</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+        
+        <li class="nav-item mx-2 pt-2">
+          <a class="nav-link active" id="nav-font" aria-current="page" href="#">For you</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+
+        <li class="nav-item mx-2 pt-2">
+          <a class="nav-link active" id="nav-font" aria-current="page" href="#">Wish List</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
+
+        
+    
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form class="d-flex px-3">
+
+        <ul class="navbar-nav me-3 mb-lg-0">
+            <li class="nav-item mx-2 pt-2">
+            <a class="nav-link active" id="nav-font" aria-current="page" href="#">My Account</a>
+            </li>
+        </ul>
+
+        <button id="login-btn" class="btn btn-outline-light" type="submit">Login / Sign Up</button>
       </form>
     </div>
   </div>
 </nav>
+
     </div>
     <router-view/>
 </template>
 
 <style>
+/*#nav-logo {
+    width: 50px;
+    height: 50px;
+} */
+
+#navbar {
+    background-image: linear-gradient(to bottom right,#4B279B,#DF99D8);
+
+}
+
+login-btn {
+    font-size: 20px;
+}
+
+#nav-font {
+    color: white;
+    font-size: 20px;
+    
+}
+
+#nav-logo-font {
+    color: white;
+    font-size: 30px;
+    font-family: 'Asul', sans-serif;
+}
+
 
 
 </style>
