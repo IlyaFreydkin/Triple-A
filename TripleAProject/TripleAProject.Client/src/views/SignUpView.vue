f715d23008f3602b2589d7aeaba05cf92384ba34
Ilya <fre22343@spengergasse.at>
2022-12-20 13:57:08 +0100

    signup frontend created

--- TripleAProject/TripleAProject.Client/src/views/SignUpView.vue
+++ TripleAProject/TripleAProject.Client/src/views/SignUpView.vue
@@ -3,47 +3,50 @@ import NavBar from '../components/NavBar.vue'
 
 </script>
 
+
 <template>
-        <div class="login">
+
+           <div class="signup">
 
        
 
           	<NavBar></NavBar>
 
-<div class="wrapper">
-        <div class="logo">
-            <img src="../images/logo.jpg" alt="">
-        </div>
-        <div class="text-center mt-4 name">
-            Triple A
-        </div>
-        <form class="p-3 mt-3">
-            <div class="form-field d-flex align-items-center">
-                <span class="far fa-user"></span>
-                <input type="text" name="userName" id="userName" placeholder="Username">
+    <div class="wrapper">
+            <div class="logo">
+                <img src="../images/logo.jpg" alt="">
             </div>
-            <div class="form-field d-flex align-items-center">
-                <span class="fas fa-key"></span>
-                <input type="password" name="password" id="pwd" placeholder="Password">
+            <div class="text-center mt-4 name">
+                Triple A
             </div>
-            <button class="btn mt-3">Login</button>
-        </form>
-        <div class="text-center fs-6">
-             or <br> <router-link to="signup"><a href="#">Sign up</a></router-link> 
+            <form class="p-3 mt-3">
+                <div class="form-field d-flex align-items-center">
+                    <span class="far fa-user"></span>
+                    <input type="text" name="Email" id="Email" placeholder="Email">
+                </div>
+                <div class="form-field d-flex align-items-center">
+                    <span class="far fa-user"></span>
+                    <input type="text" name="userName" id="userName" placeholder="Username">
+                </div>
+                <div class="form-field d-flex align-items-center">
+                    <span class="fas fa-key"></span>
+                    <input type="password" name="password" id="pwd" placeholder="Password">
+                </div>
+                <button class="btn mt-3">Sign Up</button>
+            </form>
+            <div class="text-center fs-6">
+                or <br> <router-link to="login"><a href="#">Login</a></router-link> 
+            </div>
         </div>
-    </div>
     
 
          </div>
 
 </template>
 
+
 <style scoped>
 
-/* Importing fonts from Google */
-
-
-/* Reseting */
 * {
     margin: 0;
     padding: 0;
@@ -145,4 +148,9 @@ body {
     }
 }
 
+
+
 </style>
+
+
+
