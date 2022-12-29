07415adc25d4dadd32e773a4cfabeb1354c70a66
Ilya <fre22343@spengergasse.at>
2022-12-29 17:18:04 +0100

    design erweiterung

--- TripleAProject/TripleAProject.Client/src/views/HomeView.vue
+++ TripleAProject/TripleAProject.Client/src/views/HomeView.vue
@@ -1,58 +1,118 @@
 <script setup>
-import NavBar from '../components/NavBar.vue'
-
+import NavBar from '../components/NavBar.vue';
 </script>
 
 <template>
+    <div id="page">
         <NavBar></NavBar>
 
-        
-      <img  src="../images/carousel-1.jpg" alt="Image">
-                    <div class="carousel-caption">
-                        <div class="container">
-                            <div class="row justify-content-center">
-                                <div class="col-md-7">
-                                    <h1 class="display-4 text-light mb-5 animated slideInDown">Welcome to the most comforting streaming Plattform</h1>
-                                    <a href="" class="btn btn-primary py-sm-3 px-sm-5">Start now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div class="d-grid gap-2 m-4 p-2">
+            <div class="leaf">
+                What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                make a type specimen book. It has survived not only five centuries, but also the
+                leap into electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum. Why do we use it? It is a long established fact
+                that a reader will be distracted by the readable content of a page when looking at
+                its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                distribution of letters, as opposed to using 'Content here, content here', making it
+                look like readable English. Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will
+                uncover many web sites still in their infancy. Various versions have evolved over
+                the years, sometimes by accident, sometimes on purpose (injected humour and the
+                like).
+            </div>
+            <div class="leaf-reverse">
+                What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                make a type specimen book. It has survived not only five centuries, but also the
+                leap into electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum. Why do we use it? It is a long established fact
+                that a reader will be distracted by the readable content of a page when looking at
+                its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                distribution of letters, as opposed to using 'Content here, content here', making it
+                look like readable English. Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will
+                uncover many web sites still in their infancy. Various versions have evolved over
+                the years, sometimes by accident, sometimes on purpose (injected humour and the
+                like).
+            </div>
+            <div class="leaf">
+                 What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                make a type specimen book. It has survived not only five centuries, but also the
+                leap into electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum. Why do we use it? It is a long established fact
+                that a reader will be distracted by the readable content of a page when looking at
+                its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                distribution of letters, as opposed to using 'Content here, content here', making it
+                look like readable English. Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will
+                uncover many web sites still in their infancy. Various versions have evolved over
+                the years, sometimes by accident, sometimes on purpose (injected humour and the
+                like).
+            </div>
+            <div class="leaf-reverse">
+                 What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                make a type specimen book. It has survived not only five centuries, but also the
+                leap into electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum. Why do we use it? It is a long established fact
+                that a reader will be distracted by the readable content of a page when looking at
+                its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                distribution of letters, as opposed to using 'Content here, content here', making it
+                look like readable English. Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will
+                uncover many web sites still in their infancy. Various versions have evolved over
+                the years, sometimes by accident, sometimes on purpose (injected humour and the
+                like).
+            </div>
+        </div>
+    </div>
 </template>
 
+
+
 <style scoped>
-          
+#page {
+    background: #4e4e4e
+        url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23c9b6e8' fill-opacity='0.86'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E")
+        center fixed;
+}
 
-
-.carousel-caption {
-    top:0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
+.leaf {
+    border-radius: 70px 10px 70px 10px;
+    background-color: #a18fbc;
+    color: #fff;
+    font-family: Poppins, sans-serif;
+    margin-bottom: 3rem;
+    padding: 3rem;
     
-    background: rgba(51, 21, 62, 0.512);
-    z-index: 1;
+box-shadow:  14px 14px 28px #414141,
+             -14px -14px 28px #5b5b5b;
 }
 
-img {
-    width: 100%;
-    height: 100%;
-   
-}
-
-.btn {
-    font-weight: 500;
+.leaf-reverse {
+    border-radius: 10px 70px 10px 70px;
+    background-color: #a18fbc;
+    color: #fff;
+    font-family: Poppins, sans-serif;
+    margin-bottom: 3rem;
+    padding: 3rem;
     
-        background-color: rgba(112, 51, 135, 0.816);
-        border-block-color: #4B279B;
-        border-radius: 40px;
+box-shadow:  14px 14px 28px #414141,
+             -14px -14px 28px #5b5b5b;
 }
-
-
-
-
 </style>
