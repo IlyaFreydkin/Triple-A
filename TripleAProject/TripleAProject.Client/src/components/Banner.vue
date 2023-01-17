8c9bdd6e479cbff958051845e4e3b772f6d7bf17
Uros Veljic <vel22675@spengergasse.at>
2023-01-17 09:09:11 +0100

    small changes

--- TripleAProject/TripleAProject.Client/src/components/Banner.vue
+++ TripleAProject/TripleAProject.Client/src/components/Banner.vue
@@ -1,66 +1,53 @@
 <template>
-    <div class="banner_section layout_padding">
-            <div class="container-fluid">
-               <section class="slide-wrapper">
-                  <div class="container-fluid">
-                     <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                           <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                           <li data-target="#myCarousel" data-slide-to="1"></li>
-                           <li data-target="#myCarousel" data-slide-to="2"></li>
-                           <li data-target="#myCarousel" data-slide-to="3"></li>
-                        </ol>
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner">
-                           <div class="carousel-item active">
-                              <div class="container">
-                                 <h1 class="banner_taital">Marriage House</h1>
-                                 <p class="banner_text">It is a long established fact that a reader will be distracted by the readable content of a page</p>
-                                 <div class="btn_main">
-                                    <div class="contact_bt"><a href="#">Contact Us</a></div>
-                                    <div class="readmore_bt active"><a href="#">Read More</a></div>
-                                 </div>
-                                 <div class="number_text">01</div>
-                              </div>
-                           </div>
-                           <div class="carousel-item">
-                              <div class="container">
-                                 <h1 class="banner_taital">Marriage House</h1>
-                                 <p class="banner_text">It is a long established fact that a reader will be distracted by the readable content of a page</p>
-                                 <div class="btn_main">
-                                    <div class="contact_bt"><a href="#">Contact Us</a></div>
-                                    <div class="readmore_bt active"><a href="#">Read More</a></div>
-                                 </div>
-                                 <div class="number_text">02</div>
-                              </div>
-                           </div>
-                           <div class="carousel-item">
-                              <div class="container">
-                                 <h1 class="banner_taital">Marriage House</h1>
-                                 <p class="banner_text">It is a long established fact that a reader will be distracted by the readable content of a page</p>
-                                 <div class="btn_main">
-                                    <div class="contact_bt"><a href="#">Contact Us</a></div>
-                                    <div class="readmore_bt active"><a href="#">Read More</a></div>
-                                 </div>
-                                 <div class="number_text">03</div>
-                              </div>
-                           </div>
-                           <div class="carousel-item">
-                              <div class="container">
-                                 <h1 class="banner_taital">Marriage House</h1>
-                                 <p class="banner_text">It is a long established fact that a reader will be distracted by the readable content of a page</p>
-                                 <div class="btn_main">
-                                    <div class="contact_bt"><a href="#">Contact Us</a></div>
-                                    <div class="readmore_bt active"><a href="#">Read More</a></div>
-                                 </div>
-                                 <div class="number_text">04</div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </section>
-            </div>
-         </div>
+
+
+<div class="banner">
+    <div class="container">
+        <div class="banner-content">
+            <span>NEW ARRIVALS</span>
+            <h1>TRIPLE A</h1>
+            <p>Loremipsum lorem ipsun
+
+            </p>
+            <a href="#">aaa</a>
+              <a href="#">Schauen</a>
+        </div>
+    </div>
+</div>
+
 </template>
+
+<style scoped>
+
+.banner{
+    background-image: url(../images/thor.jpg);
+    width: 100%;
+    height: 70vh;
+
+
+}
+
+.banner-content{
+    color:#fff;
+    width:55%;
+    padding-top: 170px;
+}
+
+.banner-content span{
+font-size: 18px;
+letter-spacing:3px;
+font-weight: 500;
+
+
+}
+
+.banner-content h1{
+    font-size: 60px;
+    font-weight: 600;
+    letter-spacing: 3px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-family: sans-serif;
+}
+
+</style>
