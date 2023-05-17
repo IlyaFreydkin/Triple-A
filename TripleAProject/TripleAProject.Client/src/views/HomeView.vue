a27e54e3991247bea1d87d3cd53b5f205bf9716b
Ahmed Mohamed <ahm22106@spengergasse.at>
2023-05-17 13:39:29 +0200

    login fixed

--- TripleAProject/TripleAProject.Client/src/views/HomeView.vue
+++ TripleAProject/TripleAProject.Client/src/views/HomeView.vue
@@ -8,52 +8,76 @@ import Card from '../components/Card.vue';
 <template>
     <div id="page">
         <NavBar></NavBar>
-   
-    
-       <Banner></Banner>
+        <Banner></Banner>
 
-
-
-    <div class="container">
-        <div class="row">
-            <div class="col-3">
-                <Card></Card>
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <Card>
+                        <img :src="card1Image" alt="Card 1">
+                    </Card>
+                </div>
+                <div class="col-3">
+                    <Card>
+                        <img :src="card2Image" alt="Card 2">
+                    </Card>
+                </div>
+                <div class="col-3">
+                    <Card>
+                        <img :src="card3Image" alt="Card 3">
+                    </Card>
+                </div>
+                <div class="col-3">
+                    <Card>
+                        <img :src="card4Image" alt="Card 4">
+                    </Card>
+                </div>
             </div>
-            <div class="col-3">
-                <Card></Card>
-            </div>
-            <div class="col-3">
-                <Card></Card>
-            </div>
-            <div class="col-3">
-                <Card></Card>
+            <div class="row">
+                <div class="col-3">
+                    <Card>
+                        <img :src="card5Image" alt="Card 5">
+                    </Card>
+                </div>
+                <div class="col-3">
+                    <Card>
+                        <img :src="card6Image" alt="Card 6">
+                    </Card>
+                </div>
+                <div class="col-3">
+                    <Card>
+                        <img :src="card7Image" alt="Card 7">
+                    </Card>
+                </div>
+                <div class="col-3">
+                    <Card>
+                        <img :src="card8Image" alt="Card 8">
+                    </Card>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-3">
-                <Card></Card>
-            </div>
-            <div class="col-3">
-                <Card></Card>
-            </div>
-            <div class="col-3">
-                <Card></Card>
-            </div>
-            <div class="col-3">
-                <Card></Card>
-            </div>
-        </div>
 
-
-      
-      
+        <Footer></Footer>
     </div>
-
-    
-    </div>
-
-    <Footer></Footer>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            card1Image: '../images/card1.jpg',
+            card2Image: '../images/card2.jpg',
+            card3Image: '../images/card3.jpg',
+            card4Image: '../images/card4.jpg',
+            card5Image: '../images/card5.jpg',
+            card6Image: '../images/card6.jpg',
+            card7Image: '../images/card7.jpg',
+            card8Image: '../images/card8.jpg',
+        };
+    },
+};
+</script>
+
 
 
 
